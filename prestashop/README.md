81682ad87ff94a946f944dacdc7611426829909a
J. Francisco <jfm.lisaso@gmx.com>
2019-10-03 18:14:52 +0200

    Tests añadidos

--- prestashop/README.md
+++ prestashop/README.md
@@ -2,17 +2,21 @@
 
 ## Description
 
-This cookbook prepares the PrestaShop software to be installed in a virtual server. There're two possibilities to do so:
+This cookbook prepares the PrestaShop software to be installed. There're two possibilities to do so:
 
-1) If the attribute `node['prestashop']['use_docker']` is true, it launches two _docker_ containers; one with a MySQL database and another one with prestashop.
+1) If the attribute `node['prestashop']['use_docker']` is true, it launches two _docker_ containers; one with a MySQL database and another one with PrestaShop.
 
 2) Otherwise, the cookbook installs an Apache web server, MariaDB, the required PHP modules and it downloads and extracts the PrestaShop software in the correct directory.
 
-In any case you must install PrestaShop manually by going to http://\<hostname\>/prestashop/install.php
+In any case you must install PrestaShop manually by going to `http://\<hostname\>/prestashop/install.php`
 
 ## Usage
 
 Add the "prestashop" recipe to your node's run list or role, or include the recipe in another cookbook.
+
+## Tests
+
+Launch the command `kitchen verify` to launch the smoke tests for the cookbook.
 
 ## License and Author
 
--- prestashop/test/integration/default/default_test.rb
+++ prestashop/test/integration/default/default_test.rb
@@ -5,28 +5,48 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-describe package('apache2') do
-  it { should be_installed }
-end
-
-# This is an example test, replace with your own test.
-describe user('root') do
-  it { should exist }
-end
-
-# This is an example test, replace it with your own test.
 describe port(80) do
   it { should be_listening }
 end
 
-
-require 'spec_helper'
-
 describe file('/usr/bin/docker') do
+  let(:node) { json('/tmp/kitchen/nodes/default-ubuntu-1804.json').default }
+  before do
+    skip if node['prestashop']['use_docker'] == false
+  end
   it { should be_file }
   it { should be_executable }
 end
 
-describe command('/usr/bin/docker version') do
-  its(:stdout) { should match /Version:      1.12.1/ }
+# check whether Docker daemon is running:
+describe command('systemctl is-active docker') do
+  let(:node) { json('/tmp/kitchen/nodes/default-ubuntu-1804.json').default }
+  before do
+    skip if node['prestashop']['use_docker'] == false
+  end
+  its(:stdout) { should match /active/ }
+end
+
+describe docker_container('mysql_container') do
+  let(:node) { json('/tmp/kitchen/nodes/default-ubuntu-1804.json').default }
+  before do
+    skip if node['prestashop']['use_docker'] == false
+  end
+  it { should exist }
+  it { should be_running }
+  its('repo') { should eq 'mysql' }
+  its('ports') { should include "0.0.0.0:3306->3306/tcp" }
+end
+
+# check whether Prestashop container is running
+# check whether Prestashop is listening on port 80
+describe docker_container('prestashop_container_a') do
+  let(:node) { json('/tmp/kitchen/nodes/default-ubuntu-1804.json').default }
+  before do
+    skip if node['prestashop']['use_docker'] == false
+  end
+  it { should exist }
+  it { should be_running }
+  its('repo') { should eq 'prestashop/prestashop' }
+  its('ports') { should include "0.0.0.0:80->80/tcp" }
 end
